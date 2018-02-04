5152ed8a2f28add6bd8fce40341cb0c7d8aec01f
Sasha <vdovicenco.alexandr@gmail.com>
2018-02-04 13:17:54 +0200

    Initial Commit

---
+++ MeetennisTests/MeetennisTests.swift
@@ -0,0 +1,36 @@
+//
+//  MeetennisTests.swift
+//  MeetennisTests
+//
+//  Created by Hackintosh on 2/4/18.
+//  Copyright © 2018 Hackintosh. All rights reserved.
+//
+
+import XCTest
+@testable import Meetennis
+
+class MeetennisTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
