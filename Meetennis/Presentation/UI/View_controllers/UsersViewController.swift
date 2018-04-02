a8f51b171900a5b557555a73b578709da54f8887
Sasha <vdovicenco.alexandr@gmail.com>
2018-04-02 03:15:31 +0300

    Added Tab Bar and performed navigation

---
+++ Meetennis/Presentation/UI/View_controllers/UsersViewController.swift
@@ -0,0 +1,23 @@
+//
+//  UsersViewController.swift
+//  Meetennis
+//
+//  Created by Hackintosh on 4/2/18.
+//  Copyright © 2018 Hackintosh. All rights reserved.
+//
+
+import UIKit
+
+class UsersViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //setPresenter()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
